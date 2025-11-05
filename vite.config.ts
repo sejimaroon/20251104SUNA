52685cade791e378346275bce233c276a29a711b
sejimaroon <jman128@docomo.ne.jp>
2025-11-05 14:41:00 +0900

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/20251104SUNA/',
+  base: '/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
